3da7ad0c07bf674ed426bbdaa939d432e474143c
harshanand9891 <130561407+harshanand9891@users.noreply.github.com>
2023-09-22 12:19:34 +0530

    Add files via upload

---
+++ stock_price_prediction.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import math
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+
+
+# In[13]:
+
+
+df = yf.download('AAPL', start='2010-01-01', end='2023-05-28')
+
+plt.style.use('fivethirtyeight')
+
+
+
+# In[14]:
+
+
+# Load the dataset
+df.to_csv('AAPL_Stock_Price.csv')
+
+
+# Select the 'Close' column as the target variable
+data = df.filter(['Close'])
+
+# Convert the dataframe to a numpy array
+dataset = data.values
+
+# Scale the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+# Create training dataset
+training_data_len = math.ceil(len(dataset) * 0.8)
+train_data = scaled_data[0:training_data_len, :]
+
+# Split the data into input (x) and output (y) sets
+x_train = []
+y_train = []
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+
+# Convert the x_train and y_train to numpy arrays
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+
+# In[15]:
+
+
+# Reshape the data for LSTM model (input shape [samples, time steps, features])
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# Create the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+
+# In[16]:
+
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=10)
+
+
+# In[17]:
+
+
+# Prepare the test dataset
+test_data = scaled_data[training_data_len - 60:, :]
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+# Convert the test data to numpy array and reshape
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+
+# In[18]:
+
+
+# Get the model's predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+
+# In[20]:
+
+
+# Calculate the RMSE
+rmse = np.sqrt(np.mean((predictions - y_test) ** 2))
+print("Root Mean Squared Error (RMSE):", rmse)
+
+
+
+
+
+# In[21]:
+
+
+# Plot the graph
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+
+plt.figure(figsize=(8, 4))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price', fontsize=18)
+plt.plot(train['Close'],linewidth = '1')
+plt.plot(valid[['Close', 'Predictions']],linewidth = '1')
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.show()
+
+
+# In[22]:
+
+
+# Predict the next day's closing price
+last_60_days = scaled_data[-60:]
+X_test = np.array([last_60_days])
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+pred_price = model.predict(X_test)
+pred_price = scaler.inverse_transform(pred_price)
+print("Predicted price for the next day:", pred_price[0][0])
+
+
+
+# In[24]:
+
+
+# Evaluate the model on the test data
+test_data_accuracy = r2_score(y_test, predictions)
+print("Test Data Accuracy (R-squared):", test_data_accuracy)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
